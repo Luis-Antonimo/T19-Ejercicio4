baa441a358fe7b43b7b8ff761c1030921d235b56
Luis Antónimo <luisantonimo@gmail.com>
2022-07-29 17:55:27 +0200

    ok

---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T19_Ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        double valorA;
+        string calculo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text == "0" && Pantalla.Text != null)
+            {
+                Pantalla.Text = "4";
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "4";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text == "0" && Pantalla.Text != null)
+            {
+                Pantalla.Text = "1";
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "1";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text == "0" && Pantalla.Text != null)
+            {
+                Pantalla.Text = "2";
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "2";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text == "0" && Pantalla.Text != null)
+            {
+                Pantalla.Text = "3";
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "3";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text == "0" && Pantalla.Text != null)
+            {
+                Pantalla.Text = "5";
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "5";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text == "0" && Pantalla.Text != null)
+            {
+                Pantalla.Text = "6";
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "6";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text == "0" && Pantalla.Text != null)
+            {
+                Pantalla.Text = "7";
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "7";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text == "0" && Pantalla.Text != null)
+            {
+                Pantalla.Text = "8";
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "8";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text == "0" && Pantalla.Text != null)
+            {
+                Pantalla.Text = "9";
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "9";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "0";
+        }
+
+        private void Pantalla_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            valorA = Convert.ToDouble(Pantalla.Text);
+            Pantalla.Text = "0";
+            calculo = "/";
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = "0";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            valorA = Convert.ToDouble(Pantalla.Text);
+            Pantalla.Text = "0";
+            calculo = "+";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            valorA = Convert.ToDouble(Pantalla.Text);
+            Pantalla.Text = "0";
+            calculo = "*";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            valorA = Convert.ToDouble(Pantalla.Text);
+            Pantalla.Text = "0";
+            calculo = "-";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            double valorB;
+            double salida;
+
+            valorB = Convert.ToDouble(Pantalla.Text);
+
+            if (calculo == "+")
+            {
+                salida = (valorA + valorB);
+                Pantalla.Text = Convert.ToString(salida);
+                valorA = salida;
+            }
+            if (calculo == "-")
+            {
+                salida = (valorA - valorB);
+                Pantalla.Text = Convert.ToString(salida);
+                valorA = salida;
+            }
+            if (calculo == "*")
+            {
+                salida = (valorA * valorB);
+                Pantalla.Text = Convert.ToString(salida);
+                valorA = salida;
+            }
+            if (calculo == "/")
+            {
+                 salida = (valorA / valorB);
+                 Pantalla.Text = Convert.ToString(salida);
+                 valorA = salida;              
+            }
+        }
+    }
+}
